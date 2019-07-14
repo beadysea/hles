9f0cb5b1918979e099260f8197f2c8c99df855a3
Bill Crawford <3253399+beadysea@users.noreply.github.com>
2019-07-14 20:33:33 +0100

    update sphinx makefiles

--- docsrc/make.bat
+++ docsrc/make.bat
@@ -8,7 +8,7 @@ if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
 set SOURCEDIR=../docsrc
-set BUILDDIR=build
+set BUILDDIR=_build
 REM GITHUBDIR is used in a windows command so \ used instead of /
 set GITHUBDIR=..\docs
 
